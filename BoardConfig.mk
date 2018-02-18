d723b3360ebd1c20945a4a4ee0cc94b943033fac
Hemant Sharma <hemantbeast@gmail.com>
2018-02-19 00:07:03 +0530

    armani: Add initial manifest and compatibility matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,10 @@ BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw/
 # Headers
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/hidl/manifest.xml
+DEVICE_MATRIX_FILE := $(LOCAL_PATH)/hidl/compatibility_matrix.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_armani
 TARGET_RECOVERY_DEVICE_MODULES := libinit_armani
