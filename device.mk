f049770092709810c92f8f8139e869af4ff2e048
Hemant Sharma <hemantbeast@gmail.com>
2018-03-14 22:02:55 +0530

    Revert "armani: Enforce RRO on framework-res"
    This reverts commit 1ad0087cd2a142cff0a8a291691548c2106555ff.

--- device.mk
+++ device.mk
@@ -181,9 +181,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=/vendor/lib/libqti-perfd-client.so
 
